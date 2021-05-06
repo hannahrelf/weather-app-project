22ae24a1693dc6c490617d4262cec47503ca6585
hannahrelf <82025576+hannahrelf@users.noreply.github.com>
2021-05-06 08:23:05 +0100

    Added weather icon

--- src/index.js
+++ src/index.js
@@ -37,6 +37,12 @@ function showWeather(response) {
   document.querySelector("#humidity").innerHTML = `${Math.round(
     response.data.main.humidity
   )} %`;
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", "response.data.weather[0].description");
 }
 
 function searchCity(city) {
